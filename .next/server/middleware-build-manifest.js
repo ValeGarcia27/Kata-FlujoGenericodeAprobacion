f8f25eb3d473a5c31134e6dab13d960cc2378e40
ValeGarcia27 <valentinagarciaosorio5@gmail.com>
2025-08-26 02:22:07 -0500

    cargue

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-4328cfe29c4e4124.js","static/chunks/4bd1b696-2385f465b218746f.js","static/chunks/964-e0ba4b8b29a30c80.js","static/chunks/main-app-0b6065f9b5cbc253.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-4328cfe29c4e4124.js","static/chunks/framework-768692517470e708.js","static/chunks/main-9d70a0782ec3047b.js","static/chunks/pages/_app-663ec5428c344dae.js"],"/_error":["static/chunks/webpack-4328cfe29c4e4124.js","static/chunks/framework-768692517470e708.js","static/chunks/main-9d70a0782ec3047b.js","static/chunks/pages/_error-544778206352ce59.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
---
+++ .next/static/webpack/app/historial/page.26da1f4e1d12784a.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/historial/page",{
+
+/***/ "(app-pages-browser)/./src/styles/HistorialPorSolicitud.css":
+/*!**********************************************!*\
+  !*** ./src/styles/HistorialPorSolicitud.css ***!
+  \**********************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"fd521cb68017\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9zdHlsZXMvSGlzdG9yaWFsUG9yU29saWNpdHVkLmNzcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsaUVBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIkM6XFxVc2Vyc1xcVmFsZW50aW5hXFxPbmVEcml2ZVxcRGVza3RvcFxcS2F0YSBHaXRcXEthdGEtRmx1am9HZW5lcmljb2RlQXByb2JhY2lvblxcc3JjXFxzdHlsZXNcXEhpc3RvcmlhbFBvclNvbGljaXR1ZC5jc3MiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCJmZDUyMWNiNjgwMTdcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/styles/HistorialPorSolicitud.css\n"));
+
+/***/ })
+
+});
